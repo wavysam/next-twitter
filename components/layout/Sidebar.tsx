a8c4fd2b8fa14865bc4892937f2436be31bc9012
Samuel Esplana <samuelesplana.dev@gmail.com>
2023-09-26 17:58:26 +0800

    edit sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,25 +1,42 @@
-import { getAuthSession } from "@/lib/auth";
+"use client";
+
+import { RiHome7Fill, RiHome7Line } from "react-icons/ri";
+import { HiOutlineUser, HiUser } from "react-icons/hi";
+import { TbBell, TbBellFilled } from "react-icons/tb";
+import { Session } from "next-auth";
+
 import SidebarItem from "./SidebarItem";
 import SidebarLogo from "./SidebarLogo";
 
-const SideBar = async () => {
-  const session = await getAuthSession();
+type SidebarProps = {
+  session: Session | null;
+};
 
+const SideBar = ({ session }: SidebarProps) => {
   return (
-    <aside className="relative h-full md:ml-12">
-      <div className="flex flex-col items-start">
-        <div className="space-y-2">
-          <div className="flex justify-start">
-            <SidebarLogo />
-          </div>
-          <SidebarItem
-            sessionUsername={session?.user.username}
-            sessionName={session?.user.name}
-            sessionImage={session?.user.image}
-          />
-        </div>
+    <div className="relative h-full w-full">
+      <div className="flex flex-col items-start space-y-2">
+        <SidebarLogo />
+        <SidebarItem
+          label="Home"
+          href="/home"
+          icon={RiHome7Line}
+          activeIcon={RiHome7Fill}
+        />
+        <SidebarItem
+          label="Notifications"
+          href="/notifications"
+          icon={TbBell}
+          activeIcon={TbBellFilled}
+        />
+        <SidebarItem
+          label="Profile"
+          href={`/${session?.user.username}`}
+          icon={HiOutlineUser}
+          activeIcon={HiUser}
+        />
       </div>
-    </aside>
+    </div>
   );
 };
 
--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,123 +1,47 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { HiOutlineUser, HiUser } from "react-icons/hi";
-import { RiHome7Fill, RiHome7Line } from "react-icons/ri";
-import { TbBellFilled, TbBell } from "react-icons/tb";
-import { FiMoreHorizontal } from "react-icons/fi";
-import { signOut } from "next-auth/react";
-
-import { cn } from "@/lib/utils";
-import Avatar from "../Avatar";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { IconType } from "react-icons";
 
 type SidebarItemProps = {
-  sessionUsername: string | undefined;
-  sessionName: string | null | undefined;
-  sessionImage: string | null | undefined;
+  label: string;
+  href: string;
+  icon: IconType;
+  activeIcon: IconType;
 };
 
 const SidebarItem = ({
-  sessionUsername,
-  sessionName,
-  sessionImage,
+  label,
+  href,
+  icon: Icon,
+  activeIcon: ActiveIcon,
 }: SidebarItemProps) => {
   const pathname = usePathname();
-
+  const isActive = pathname === href;
   return (
-    <div className="relative min-h-screen w-full">
-      <div className="flex flex-col justify-between items-center">
-        <div className="flex flex-col space-y-2 items-start">
-          <Link
-            href="/home"
-            className="hover:bg-neutral-200 rounded-full p-3 transition"
+    <div className="relative h-full w-full">
+      <div className="flex flex-col items-start space-y-2">
+        <Link
+          href={href}
+          className="flex items-center space-x-3 justify-start p-3 hover:bg-neutral-200 rounded-full transition"
+        >
+          {isActive ? (
+            <ActiveIcon className="h-7 w-7" />
+          ) : (
+            <Icon className="h-7 w-7" />
+          )}
+
+          <span
+            className={cn(
+              "hidden md:flex text-xl",
+              isActive ? "font-semibold" : "font-normal"
+            )}
           >
-            <div className="flex items-center gap-3">
-              {pathname === "/home" ? (
-                <RiHome7Fill className="h-7 w-7" />
-              ) : (
-                <RiHome7Line className="h-7 w-7" />
-              )}
-              <span
-                className={cn(
-                  "text-xl hidden md:block",
-                  pathname === "/home" ? "font-semibold" : "font-normal"
-                )}
-              >
-                Home
-              </span>
-            </div>
-          </Link>
-          <Link
-            href="/notifications"
-            className="hover:bg-neutral-200 rounded-full p-3 transition"
-          >
-            <div className="flex items-center gap-3">
-              {pathname === "/notifications" ? (
-                <TbBellFilled className="h-7 w-7" />
-              ) : (
-                <TbBell className="h-7 w-7" />
-              )}
-              <span
-                className={cn(
-                  "text-xl hidden md:block",
-                  pathname === "/notifications"
-                    ? "font-semibold"
-                    : "font-normal"
-                )}
-              >
-                Notifications
-              </span>
-            </div>
-          </Link>
-          <Link
-            href={`/${sessionUsername}`}
-            className="hover:bg-neutral-200 rounded-full p-3 transition"
-          >
-            <div className="flex items-center gap-3">
-              {pathname === `/${sessionUsername}` ? (
-                <HiUser className="h-7 w-7" />
-              ) : (
-                <HiOutlineUser className="h-7 w-7" />
-              )}
-              <span
-                className={cn(
-                  "text-xl hidden md:block",
-                  pathname === `/${sessionUsername}`
-                    ? "font-semibold"
-                    : "font-normal"
-                )}
-              >
-                Profile
-              </span>
-            </div>
-          </Link>
-        </div>
-        <div className="absolute bottom-16 left-0 flex items-start">
-          <Popover>
-            <PopoverTrigger asChild>
-              <div className="flex flex-nowrap items-center justify-center gap-2 md:w-[250px] p-1.5 md:py-3 hover:bg-neutral-200 rounded-full cursor-pointer transition">
-                <Avatar src={sessionImage!} />
-                <div className="hidden md:flex md:flex-col">
-                  <p className="text-sm font-semibold">{sessionName}</p>
-                  <p className="text-sm text-neutral-500">@{sessionUsername}</p>
-                </div>
-                <FiMoreHorizontal className="hidden md:flex" />
-              </div>
-            </PopoverTrigger>
-            <PopoverContent className="p-0 m-0 h-20 flex items-center rounded-md">
-              <div
-                className="cursor-pointer hover:bg-neutral-100 w-full py-3"
-                onClick={() => signOut({ callbackUrl: "/" })}
-              >
-                <p className="px-6 text-lg font-medium">
-                  Log out @{sessionUsername}
-                </p>
-              </div>
-            </PopoverContent>
-          </Popover>
-        </div>
+            {label}
+          </span>
+        </Link>
       </div>
     </div>
   );
